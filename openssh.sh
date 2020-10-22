3b489b12376b955db1a615e999c1cfe47a2fccc9
Nanda Gunawan <nandakanaksasak@gmail.com>
2020-10-22 10:29:11 +0700

    QueenSSH

--- openssh.sh
+++ openssh.sh
@@ -126,17 +126,17 @@ wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/gugun09/tunnel_
 sed -i $MYIP2 /etc/squid/squid.conf;
 /etc/init.d/squid restart
 
-echo "=================  saya matikan install Webmin  ======================"
+echo "=================  install Webmin  ======================"
 echo "========================================================="
 
 # install webmin
 cd
-#wget http://prdownloads.sourceforge.net/webadmin/webmin_1.910_all.deb
-#dpkg --install webmin_1.910_all.deb;
-#apt-get -y -f install;
-#sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-#rm -f webmin_1.910_all.deb
-#/etc/init.d/webmin restart
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.910_all.deb
+dpkg --install webmin_1.910_all.deb;
+apt-get -y -f install;
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm -f webmin_1.910_all.deb
+/etc/init.d/webmin restart
 
 echo "=================  install stunnel  ====================="
 echo "========================================================="
@@ -324,6 +324,82 @@ echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/gugun09/tunnel_9-10/main/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Setup by QueenSSH</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/gugun09/tunnel_9-10/main/vps.conf"
+
+# install openvpn
+apt-get -y install openvpn easy-rsa openssl
+cp -r /usr/share/easy-rsa/ /etc/openvpn
+mkdir /etc/openvpn/easy-rsa/keys
+sed -i 's|export KEY_COUNTRY="US"|export KEY_COUNTRY="ID"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_PROVINCE="CA"|export KEY_PROVINCE="Samarinda"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_CITY="SanFrancisco"|export KEY_CITY="Samarinda"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_ORG="Fort-Funston"|export KEY_ORG="ZonaNyaman"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_EMAIL="me@myhost.mydomain"|export KEY_EMAIL="Gtatisan09@gmail.com"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_OU="MyOrganizationalUnit"|export KEY_OU="IpangNettNott"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_NAME="EasyRSA"|export KEY_NAME="GUGUN"|' /etc/openvpn/easy-rsa/vars
+sed -i 's|export KEY_OU=changeme|export KEY_OU=GUGUN|' /etc/openvpn/easy-rsa/vars
+
+# Create Diffie-Helman Pem
+openssl dhparam -out /etc/openvpn/dh2048.pem 2048
+
+# Create PKI
+cd /etc/openvpn/easy-rsa
+cp openssl-1.0.0.cnf openssl.cnf
+. ./vars
+./clean-all
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" --initca $*
+
+# Create key server
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" --server server
+
+# Setting KEY CN
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" client
+
+# cp /etc/openvpn/easy-rsa/keys/{server.crt,server.key,ca.crt} /etc/openvpn
+cd
+cp /etc/openvpn/easy-rsa/keys/server.crt /etc/openvpn/server.crt
+cp /etc/openvpn/easy-rsa/keys/server.key /etc/openvpn/server.key
+cp /etc/openvpn/easy-rsa/keys/ca.crt /etc/openvpn/ca.crt
+chmod +x /etc/openvpn/ca.crt
+
+# server settings
+cd /etc/openvpn/
+wget -O /etc/openvpn/server.conf "https://raw.githubusercontent.com/gugun09/tunnel_9-10/main/server.conf"
+systemctl start openvpn@server
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables.up.rules
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/gugun09/tunnel_9-10/main/iptables-openvpn"
+chmod +x /etc/network/if-up.d/iptables
+sed -i 's|LimitNPROC|#LimitNPROC|g' /lib/systemd/system/openvpn@.service
+systemctl daemon-reload
+/etc/init.d/openvpn restart
+
+# openvpn config
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/gugun09/tunnel_9-10/main/client.conf"
+sed -i $MYIP2 /etc/openvpn/client.ovpn;
+echo '<ca>' >> /etc/openvpn/client.ovpn
+cat /etc/openvpn/ca.crt >> /etc/openvpn/client.ovpn
+echo '</ca>' >> /etc/openvpn/client.ovpn
+cp client.ovpn /home/vps/public_html/
+#wget -O /etc/openvpn/openvpnssl.ovpn "https://raw.githubusercontent.com/gugun09/install/master/openvpnssl.conf"
+#echo '<ca>' >> /etc/openvpn/openvpnssl.ovpn
+#cat /etc/openvpn/ca.crt >> /etc/openvpn/openvpnssl.ovpn
+#echo '</ca>' >> /etc/openvpn/openvpnssl.ovpn
+#cp openvpnssl.ovpn /home/vps/public_html/
+
 # download script
 cd /usr/bin
 wget -O menu "https://raw.githubusercontent.com/gugun09/tunnel_9-10/main/menu.sh"
@@ -350,7 +426,7 @@ echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
 
 chmod +x menu
 chmod +x usernew
-chmod +x add-pptp
+chmod +x pptp
 chmod +x delete-pptp
 chmod +x alluser-pptp
 chmod +x login-pptp
