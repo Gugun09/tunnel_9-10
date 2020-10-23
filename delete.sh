f4cdc6b05d107e9f01e1f28354e0596476401b5a
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2020-10-24 01:21:44 +0700

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-# ===========================================
-# | AUTOSCRIPT FOR INSTALLATION SSH AND VPN |
-# ===========================================
-# | Owner : Administrator JasonTheKiller    |
-# | Date Owned : 12/10/2018                 |
-# | Telegram : t.me/JasonKiller             |
-# ===========================================
             
             echo ""
 
                if [ ! -f /usr/local/bin/deleteduser ]; then
-               echo "echo "Autoscript by JasonTheKiller"" > /usr/local/bin/deleteduser
+               echo "echo "Autoscript by QueenSSH"" > /usr/local/bin/deleteduser
                chmod +x /usr/local/bin/deleteduser
                fi
                hariini=`date +%d-%m-%Y`
@@ -36,13 +29,13 @@
                username=$username" " 
                done
                bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
-               echo "echo "JasonTheKiller- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
+               echo "echo "QueenSSH- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
                todaystime=`date +%s`
                if [ $userexpireinseconds -ge $todaystime ] ;
                then
 		    	:
                else
-               echo "echo "JasonTheKiller- Username : $username are expired at: $tgl $bulantahun and removed : $hariini "" >> /usr/local/bin/deleteduser
+               echo "echo "QueenSSH- Username : $username are expired at: $tgl $bulantahun and removed : $hariini "" >> /usr/local/bin/deleteduser
 	           echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
                userdel $username
                fi
