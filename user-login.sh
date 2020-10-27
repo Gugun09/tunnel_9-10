0b6492e14d3ead2fa6516fec21b7d6ebb5597a6d
Nanda Gunawan <54890927+Gugun09@users.noreply.github.com>
2020-10-27 18:10:32 +0700

    Update user-login.sh

--- user-login.sh
+++ user-login.sh
@@ -44,6 +44,17 @@ done
 
 echo "";
 
+if [ -f "/etc/openvpn/server-tcp-2200.log" ]; then
+	line=`cat /etc/openvpn/server-tcp-2200.log | wc -l`
+	a=$((3+((line-8)/2)))
+	b=$(((line-8)/2))
+echo "----------------------";
+echo "OpenVPN User Login";
+echo "----------------------";
+	cat /etc/openvpn/server-tcp-2200.log | head -n $a | tail -n $b | cut -d "," -f 1,2,5 | sed -e 's/,/   /g' > /tmp/vpn-login-db.txt
+	cat /tmp/vpn-login-db.txt
+fi
+echo " "
 echo "------------------------------------------------"
 echo " Multi Login = kill "
 echo " Cara pakai : kill nomor PID "
